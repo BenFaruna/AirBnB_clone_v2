4e1fa3cff2d187598ed5b0ee130496c91bdc4ede
BenFaruna <benjaminfaruna@gmail.com>
2022-10-16 00:56:38 +0000

    Fixed sqlalchemy constraint error

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, url_for, render_template
 
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
@@ -20,6 +21,14 @@ def states_list():
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """routing for displaying cities by their states"""
+    states = storage.all(State)
+    cities = storage.all(City)
+
+    return render_template("8-cities_by_states.html", states=states, cities=cities)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/9-states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""routing flask and displaying text"""
+from flask import Flask, redirect, url_for, render_template
+
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """closes sqlalchemy session"""
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """routing for displaying cities by their states"""
+    states = storage.all(State)
+    cities = storage.all(City)
+
+    return render_template("8-cities_by_states.html",
+                           states=states, cities=cities)
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """routing for state list"""
+    return render_template("9-states.html", states=None, cities=None)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state_with_city(id):
+    """check for cities under a state using it's id"""
+    pass
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
