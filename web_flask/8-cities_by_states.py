3cc84cb2333c9c953ad329f62e17673e92606202
BenFaruna <benjaminfaruna@gmail.com>
2022-10-16 16:24:58 +0000

    turned debug mode off

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -31,4 +31,4 @@ def cities_by_states():
     return render_template("8-cities_by_states.html", states=states, cities=cities)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
